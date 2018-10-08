780659d2be37ffb99a556c8c86b319684ffd3d32
Sam Dalton <spd4nf@virginia.edu>
2018-10-08 13:34:39 -0500

    got static server working

--- server.js
+++ server.js
@@ -7,10 +7,7 @@ const data = require('./db/notes');
 
 const app = express();
 
-app.use(express.statis('public'));
-
-// INSERT EXPRESS APP CODE HERE...
-
+app.use(express.static('public'));
 
 app.listen(8080, function(){
   console.info(`Server listening on ${this.address().port}`);
