c9178b15254893cda627baeac8bc3bf298a69ba5
Sam Dalton <spd4nf@virginia.edu>
2018-10-09 14:23:39 -0500

    added PUT Notes by ID

--- server.js
+++ server.js
@@ -17,6 +17,33 @@ app.use(logger);
 
 app.use(express.static('public'));
 
+app.use(express.json());
+
+app.put('/api/notes/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  /***** Never trust users - validate input *****/
+  const updateObj = {};
+  const updateFields = ['title', 'content'];
+
+  updateFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
+
+  notes.update(id, updateObj, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    } else {
+      next();
+    }
+  });
+});
+
 app.get('/api/notes', (req, res, next) => {
   const { searchTerm } = req.query;
 
