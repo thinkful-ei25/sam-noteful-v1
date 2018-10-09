ad475ecfe19d014160a97601b8d5c70983cd46e1
Sam Dalton <spd4nf@virginia.edu>
2018-10-09 14:11:35 -0500

    bringing in simDB as database

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 const express = require('express');
 
 const data = require('./db/notes');
+const simDB = require('./db/simDB');
+const notes = simDB.initialize(data);
 
 const logger = require('./middleware/logger');
 
