240e5e5c997a8faea6aa86ab9533cc92a2b376b4
Sam Dalton <spd4nf@virginia.edu>
2018-10-09 13:55:20 -0500

    added error handling

--- server.js
+++ server.js
@@ -39,10 +39,22 @@ app.get('/api/notes/:id', (req, res) =>{
   res.json(answer);
 });
 
+app.get('/boom', (req, res, next) => {
+  throw new Error('Boom!!');
+});
+
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   res.status(404).json({ message: 'Not Found' });
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
 });
 
 app.listen(PORT, function(){
