35db1a6841c9a058ef5dc4fc798cb5f9aafcd4f4
Sam Dalton <spd4nf@virginia.edu>
2018-10-11 11:35:24 -0500

    updated the notes.METHOD() to use promises

--- router/notes.router.js
+++ router/notes.router.js
@@ -20,43 +20,84 @@ router.put('/notes/:id', (req, res, next) => {
     }
   });
 
-  notes.update(id, updateObj, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.json(item);
-    } else {
-      next();
-    }
-  });
+  notes.update(id, updateObj)
+    .then(item=>{
+      if(item){
+        res.json(item);
+      } else {
+        next();
+      }
+    })
+    .catch(err =>{
+      next(err);
+    });
+
+
+  // notes.update(id, updateObj, (err, item) => {
+  //   if (err) {
+  //     return next(err);
+  //   }
+  //   if (item) {
+  //     res.json(item);
+  //   } else {
+  //     next();
+  //   }
+  // });
 });
 
 router.get('/notes', (req, res, next) => {
   const { searchTerm } = req.query;
 
-  notes.filter(searchTerm, (err, list) => {
-    if (err) {
-      return next(err); // goes to error handler
-    }
-    res.json(list); // responds with filtered array
-  });
+  notes.filter(searchTerm)
+    .then(item =>{
+      if(item){
+        res.json(item);
+      } else {
+        next();
+      }
+    })
+    .catch(err =>{
+      next(err);
+    });
+
+
+  // notes.filter(searchTerm, (err, list) => {
+  //   if (err) {
+  //     return next(err); // goes to error handler
+  //   }
+  //   res.json(list); // responds with filtered array
+  // });
+
 });
 
 router.get('/notes/:id', (req, res, next) =>{
   const id = req.params.id;
-  notes.find(id, (err,item) =>{
-    if(err){
-      return next(err);
-    }
-    if(item){
-      res.json(item);
-    } else {
-      next();
-    }
-  });
+  notes.find(id)
+    .then(item=> {
+      if(item){
+        res.json(item);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+
+  // notes.find(id, (err,item) =>{
+  //   if(err){
+  //     return next(err);
+  //   }
+  //   if(item){
+  //     res.json(item);
+  //   } else {
+  //     next();
+  //   }
+  // });
 
 });
+
+  
 
 // Post (insert) an item
 router.post('/notes', (req, res, next) => {
@@ -70,26 +111,52 @@ router.post('/notes', (req, res, next) => {
     return next(err);
   }
 
-  notes.create(newItem, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
-    } else {
-      next();
-    }
-  });
+  notes.create(newItem)
+    .then(item=>{
+      if(item){
+        res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+      } else {
+        next();
+      }
+    })
+    .catch(err=>{
+      next(err);
+    });
+
+  // notes.create(newItem, (err, item) => {
+  //   if (err) {
+  //     return next(err);
+  //   }
+  //   if (item) {
+  //     res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+  //   } else {
+  //     next();
+  //   }
+  // });
 });
 
 router.delete('/notes/:id', (req,res,next) =>{
   const id = req.params.id;
-  notes.delete(id, (err)=>{
-    if(err){
-      return next(err);
-    }
-    res.sendStatus(204);
-  });
+  
+  notes.delete(id)
+    .then(item=>{
+      if(item){
+        res.sendStatus(204);
+      } else {
+        next();
+      }
+    })
+    .catch(err=>{
+      next(err);
+    });
+  
+  // notes.delete(id, (err)=>{
+  //   if(err){
+  //     return next(err);
+  //   }
+  //   res.sendStatus(204);
+  // });
+
 });
 
 
